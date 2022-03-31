3a30a0e31e2a164406b4d85e0accb7b685a34b0d
Guilherme Costa Sales <guicosal@gmail.com>
2022-03-31 16:47:27 -0300

    deposit test with negative amount

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -17,4 +17,15 @@ public class AccountTests {
         Assertions.assertEquals(expectedValue, account.getBalance());
     }
 
+    @Test
+    public void depositShoultDoNothingWhenNegativeAmount() {
+        double amount = -200.0;
+        double expectedValue = 100.0;
+
+        Account account = new Account(1L, 100.0);
+        account.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, account.getBalance());
+    }
+
 }
